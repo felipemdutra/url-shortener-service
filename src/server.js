10c56cccad69363e0e146090244bb12c7609d76a
Felipe Dutra <felipemedeirosresendedutra@gmail.com>
2024-12-23 18:56:58 -0300

    feat: test post request

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 const express = require("express")
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 8000
 
 app.set("view engine", "ejs")
 
-app.get('/', (req, res) => {
-    res.render("index")
+app.get("/", (req, res) => {
+    res.render("index");
 })
 
-app.listen(8000);
+app.post("/shorten", (req, res) => {
+    const longUrl = req.body.longUrl;
+    console.log("Received: ", longUrl);
+
+    res.redirect("/");
+})
+
+app.listen(PORT);
 
