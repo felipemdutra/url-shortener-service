dbd2af0eb6ab50f616f84747b1b1f1adecd4b157
Felipe Dutra <felipemedeirosresendedutra@gmail.com>
2024-12-24 18:50:35 -0300

    implement 'closeDatabase' function

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import express from "express"
 
 import { createShortUrl } from "./url.js"
-import { initializeDatabase, insertUrl } from "./database/database.js"
+import { closeDatabase, initializeDatabase, insertUrl } from "./database/database.js"
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 const db = initializeDatabase("./urls.db")
 
@@ -13,20 +13,22 @@ const PORT = 8000
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index")
 })
 
 app.post("/shorten", (req, res) => {
-    const longUrl = req.body.longUrl;
-    console.log("Received: ", longUrl);
+    const longUrl = req.body.longUrl
+    console.log("Received: ", longUrl)
 
-    const shortUrl = createShortUrl(longUrl);
-    console.log("Shortened: ", shortUrl);
+    const shortUrl = createShortUrl(longUrl)
+    console.log("Shortened: ", shortUrl)
 
-    insertUrl(db, longUrl, shortUrl);
+    insertUrl(db, longUrl, shortUrl)
 
-    res.redirect("/");
+    res.redirect("/")
 })
 
-app.listen(PORT);
+app.listen(PORT)
+
+closeDatabase(db)
 
