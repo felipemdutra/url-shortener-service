d1de244a0382de651f5acd383cc793f14cc38b57
Felipe Dutra <felipemedeirosresendedutra@gmail.com>
2024-12-25 23:43:55 -0300

    refactor: move urls table functions to ./urls.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,8 @@
 import sqlite3 from 'sqlite3';
 
+import { initializeUrlsTable } from './urls.js';
+import { InitializeUsersTable } from './users.js';
+
 /*
  * @param {string} database path
  * @returns {sqlite3.Database}
@@ -11,24 +14,10 @@ export function initializeDatabase(dbPath) {
             return
         }
 
+        initializeUrlsTable(db);
+        InitializeUsersTable(db);
+
         console.log("Connected to the SQLite database.");
-    });
-
-    db.serialize(() => {
-        db.run(`
-                CREATE TABLE IF NOT EXISTS urls (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                original TEXT UNIQUE NOT NULL,
-                shortened TEXT UNIQUE NOT NULL
-                )
-        `, (err) => {
-            if (err) {
-                console.error("Couldn't create table:", err.message);
-                return
-            }
-        });
-
-        db.run("CREATE INDEX IF NOT EXISTS idx_shortened ON urls(shortened)");
     });
 
     return db;
@@ -42,42 +31,6 @@ export function closeDatabase(db) {
         if (err) {
             console.error("Couldn't close database:", err.message);
         }
-    })
-}
-
-/*
- * @param db {sqlite3.Database} 
- * @param original {string} original URL
- * @param shortened {string} shortened URL
- * @returns {Promise<string>}
- */
-export function insertUrl(db, original, shortened) {
-    return new Promise((resolve, reject) => {
-        db.run("INSERT INTO urls (original, shortened) VALUES (?, ?)", [original, shortened], function(err) {
-            if (err) {
-                reject(err.message);
-            } else {
-                resolve(shortened);
-            }
-        });
-    });
-}
-
-/*
-* @param db {sqlite3.Database} 
-* @param shortened {string} shortened URL
-* @returns {Promise<string>}
-*/
-export function getUrl(db, shortened) {
-    return new Promise((resolve, reject) => {
-        db.get(`SELECT original FROM urls WHERE shortened = ?`, [shortened], (err, row) => {
-                if (err) {
-                    reject(err.message)
-                    return
-                }
-
-                resolve(row ? row.original : null)
-            })
     })
 }
 
