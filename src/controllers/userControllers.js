ec05c4df0ad826b4cb6902562c7ee8114bd8bc21
Felipe Dutra <felipemedeirosresendedutra@gmail.com>
2024-12-29 14:34:28 -0300

    fix signUp function to work with expressjs post requests

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -3,19 +3,22 @@ import { insertUser, getUser } from "../database/users.js"
 /*
  * @param db {sqlite3.Database}
 */
-export async function signUp(db, req, res) {
+export function signUp(db) {
+    return async (req, res) => {
+
     const { username, email, password } = req.body
+        try {
+            await insertUser(db, username, email, password)
 
-    try {
-        await insertUser(db, username, email, password)
-        // Retrieve user info
-        const userInfo = await getUser(db, email)
+            // Retrieve user info
+            const userInfo = await getUser(db, email)
 
-        console.log("User found")
-        console.log(userInfo)
+            console.log("User found")
+            console.log(userInfo)
 
-        res.status(200).send("OK")
-    } catch (error) {
-        console.error("Error:", error);
+            res.status(200).send("OK")
+        } catch (error) {
+            console.error("Error:", error);
+        }
     }
 }
