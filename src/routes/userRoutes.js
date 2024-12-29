55568705bdc9e50552050309af0c33f1486d852a
Felipe Dutra <felipemd.dutra@gmail.com>
2024-12-29 14:41:33 -0300

    fix: use proper router name

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,15 +5,15 @@ import { signUp } from "../controllers/userControllers.js"
 export function userRouter(db) {
     const router = express.Router()
 
-    userRouter.get("/", (req, res) => {
+    router.get("/", (req, res) => {
         res.render("homePage")
     })
 
-    userRouter.get("/sign-up", (req, res) => {
+    router.get("/sign-up", (req, res) => {
         res.render("signUpPage")
     }) 
 
-    userRouter.post("/signup", signUp(db))
+    router.post("/signup", signUp(db))
 
     return router
 } 
