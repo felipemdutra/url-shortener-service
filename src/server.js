56667b2f1709c7d8f54044aa8ebe53bf0805b7ba
Felipe Dutra <felipemd.dutra@gmail.com>
2024-12-29 14:59:39 -0300

    use built-in parser

--- src/server.js
+++ src/server.js
@@ -1,26 +1,27 @@
-import express from "express"
-
-import { urlRouter } from "./routes/urlRoutes.js"
-import { userRouter } from "./routes/userRoutes.js"
-import { initializeDatabase } from "./database/database.js"
+import express from "express";
+import { urlRouter } from "./routes/urlRoutes.js";
+import { userRouter } from "./routes/userRoutes.js";
+import { initializeDatabase } from "./database/database.js";
 
 const app = express();
-app.use(express.urlencoded({ extended: true }))
 
-const db = initializeDatabase("./urlShortener.db")
+app.use(express.urlencoded({ extended: true }));  
+app.use(express.json());
 
-const PORT = 8000
+const db = initializeDatabase("./urlShortener.db");
 
-app.set("view engine", "ejs")
+const PORT = 8000;
 
-app.use("/", userRouter(db))
-app.use("/", urlRouter(db))
+app.set("view engine", "ejs");
+
+app.use("/", userRouter(db));
+app.use("/", urlRouter(db));
 
 const server = app.listen(PORT, (err) => {
     if (err) {
-        console.log(err)
-        return null
+        console.log(err);
+        return null;
     }
-    console.log("Listening on port" , PORT)
-})
+    console.log("Listening on port", PORT);
+});
 
