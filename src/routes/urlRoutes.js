ebcfa7543ef3546b07c47394cff0da2dd3ba6438
Felipe Dutra <felipemedeirosresendedutra@gmail.com>
2024-12-29 00:03:49 -0300

    change router name to 'urlRouter' to avoid confision with 'userRouter' and remove tests

--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -1,8 +1,5 @@
 import express from "express"
 
-export const router = express.Router()
+export const urlRouter = express.Router()
 
-router.get("/test/test-router", (req, res) => {
-    res.status(200).send("The router is in")
-})
 
