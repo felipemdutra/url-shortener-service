6f1d7905c9f81e58d7f0a4c6e06f8f9c909af9b1
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-01 22:59:38 -0300

    add 'comparePassword' function used to log users in

--- src/database/utils/password.js
+++ src/database/utils/password.js
@@ -5,6 +5,16 @@ import bcrypt from 'bcrypt'
  * @returns {string} encrypted password
  */
 export function hashPassword(password) {
-    return bcrypt.hashSync(password, 10)
+    return bcrypt.hashSync(password, 10);
+}
+
+/*
+ * @param password {string}
+ * @param encryptedPassword {string}
+ * @return {boolean}
+ */
+export function comparePassword(password, encryptedPassword) {
+    const result = bcrypt.compareSync(password, encryptedPassword)
+    return result ? 0 : -1;
 }
 
