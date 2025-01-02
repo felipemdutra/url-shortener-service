e8bf1891b9049f41caeb3a9c21407e1f16f74921
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-01 22:57:49 -0300

    add 'logIn' function

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import { insertUser, getUser } from "../database/users.js"
+import { comparePassword } from "../database/utils/password.js"
 
 /*
  * @param db {sqlite3.Database}
@@ -17,6 +18,27 @@ export async function signUp(req, res, db) {
 
         res.status(200)
         res.redirect("/")
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500)
+    }
+}
+
+export async function logIn(req, res, db) {
+    const { email, password } = req.body
+
+    try {
+        // Retrieve user info
+        const userInfo = await getUser(db, email)
+
+        if (userInfo < 0) {
+            return res.status(400).send("Invalid email");
+        }
+
+        if (comparePassword(password, userInfo.password) < 0) {
+            return res.status(400).send("Invalid password");
+        }
+
     } catch (error) {
         console.error("Error:", error);
         res.status(500)
