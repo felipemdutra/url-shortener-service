f9827fb6d4f891ba1330f28fcba8ac3328a1d239
Felipe Dutra <felipemd.dutra@gmail.com>
2025-01-01 22:04:59 -0300

    fix 'signUp' function to be async, add better error handling Turns out, 'signUp' needs to be async because it calls for 'getUser', that returns a promise Redirect user to main page after creating account

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -3,21 +3,23 @@ import { insertUser, getUser } from "../database/users.js"
 /*
  * @param db {sqlite3.Database}
 */
-export function signUp(req, res, db) {
+export async function signUp(req, res, db) {
     const { username, email, password } = req.body
 
     try {
         insertUser(db, username, email, password)
 
         // Retrieve user info
-        const userInfo = getUser(db, email)
+        const userInfo = await getUser(db, email)
 
         console.log("User found")
         console.log(userInfo)
 
-        res.status(200).send("OK")
+        res.status(200)
+        res.redirect("/")
     } catch (error) {
         console.error("Error:", error);
+        res.status(500)
     }
 }
 
